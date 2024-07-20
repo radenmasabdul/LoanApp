5f9678ac07f7f1134a05e998f59440916bc64844
radenmasabdul <abdul.tamsis@gmail.com>
2024-07-20 20:17:32 +0700

    First Initialization

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-7xl text-red-500">HELLO WORLD ... !</h1>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      JakartaSans: ["'Plus Jakarta Sans', sans-serif;"],
+    },
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
