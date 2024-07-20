1807992d40e0e1e742eddc218b3dcc83c56755ce
radenmasabdul <abdul.tamsis@gmail.com>
2024-07-20 21:04:31 +0700

    slicing card components

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from "../components/Navbar.vue";
+import Card from "../components/Card.vue";
 </script>
 
 <template>
   <Navbar />
+  <Card />
 </template>
 
 <style scoped></style>
